cdabc0408a55665d3ef505487ea978dc37ebcfb1
yosoynazim <yosoynazim@users.noreply.github.com>
2026-06-14 23:28:08 -0300

    chore: remove admin bootstrap

--- undiscover/undiscover/src/index.ts
+++ undiscover/undiscover/src/index.ts
@@ -1,23 +1,4 @@
 export default {
   register(/* { strapi } */) {},
-  async bootstrap({ strapi }) {
-    const email = 'nazzimciid@gmail.com';
-    const userService = strapi.service('admin::user');
-    const existing = await strapi.db.query('admin::user').findOne({ where: { email } });
-    if (!existing) {
-      const superAdminRole = await strapi.service('admin::role').getSuperAdmin();
-      if (superAdminRole) {
-        await userService.create({
-          email,
-          firstname: 'Nazim',
-          lastname: 'Cid',
-          password: 'Undiscover2025!',
-          isActive: true,
-          registrationToken: null,
-          roles: [superAdminRole.id],
-        });
-        strapi.log.info(`Admin user ${email} created`);
-      }
-    }
-  },
+  bootstrap(/* { strapi } */) {},
 }
